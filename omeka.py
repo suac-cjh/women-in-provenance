bbfe8fba86a5a30280ab0a94b69ab45142429adf
parnoud <pauline.arnoud@gmail.com>
2021-07-10 10:57:21 +0200

    added sources

--- omeka.py
+++ omeka.py
@@ -47,6 +47,9 @@ Duration:
 Event Type:
 - Type of Activity "as a" Position in "Location"
 
+For Hyperlink (sources table):
+URL: link to internet resource if it exists
+
 '''
 
 import csv
@@ -74,6 +77,8 @@ def main():
             header.extend(["Item Type Metadata:Birth Date", "Item Type Metadata:Birthplace", "Item Type Metadata:Death Date", "Item Type Metadata:Occupation", "Item Type Metadata:Biographical Text"])
         if (tableType == "activities"):
             header.extend(["Item Type Metadata:Duration", "Item Type Metadata:Event Type"])
+        if (tableType == "sources"):
+            header.extend(["Item Type Metadata:URL", "Item Type Metadata:Publication Date", "Item Type Metadata:Citation"])
 
         writer.writerow(header)
         lineCount = 0
@@ -92,7 +97,20 @@ def main():
             elif (tableType == "relationships"):
                 fullName = ((row["First"] + ' ' + row["Middle"] + ' ' + row["Last"]).replace("  ", " ").strip())
             elif (tableType == "activities"): 
-                fullName = ("Project of " + s_id[row["S_ID"]]).replace("  ", " ").strip()
+                #fullName = ("Project of " + s_id[row["S_ID"]]).replace("  ", " ").strip()
+                if row["Type of Activity"] == "Trip":
+                    location = row["Location"].split(";")[-1]
+                    fullName = ((row["Type of Activity"] + " as a " + row["Position"] + " to " + location).replace("  ", " ").strip())
+                elif row["Type of Activity"] == "Job":
+                    fullName = ((row["Type of Activity"] + " as " + row["Position"] + " at the " + row["Activity/Organization Name"]).replace("  ", " ").strip())
+                elif row["Type of Activity"] == "Project":
+                    fullName = ((row["Position"] + " for the " + row["Activity/Organization Name"] + ' ' + row["Type of Activity"]).replace("  ", " ").strip())
+                elif row["Type of Activity"] == "Education":
+                    fullName = ((row["Position"] + " at " + row["Activity/Organization Name"] + ' ' + row["Type of Activity"]).replace("  ", " ").strip())
+                else:
+                    fullName = ((row["Position"] + " of the " + row["Activity/Organization Name"]).replace("  ", " ").strip())
+            elif (tableType == "sources"):
+                fullName = row["Title"]
             else: 
                 fullName = ""
             rowVals.append(fullName)
@@ -102,6 +120,21 @@ def main():
                 rowVals.append('')
             if (tableType == "main"):
                 rowVals.append(row["Biographical Note"])
+            if (tableType == 'sources'):
+                if row["Type of Source"] != '' and row["Author"] != '' and row["Publication"] != '':
+                    rowVals.append(row["Type of Source"] + " by " + row["Author"] + " (" + row["Publication"] + ")")
+                elif row["Type of Source"] == '':
+                    if row["Author"] != '' and row["Publication"] != '':
+                        rowVals.append("by " + row["Author"] + " (" + row["Publication"] + ")")
+                    elif row["Author"] == '':
+                        rowVals.append("by " + row["Publication"])
+                    else:
+                        rowVals.append(" by " + row["Author"])
+                elif row["Author"] == '':
+                    if row["Publication"] != '':
+                        rowVals.append(row["Type of Source"] + " by " + row["Publication"])
+                    else:
+                        rowVals.append(row["Type of Source"])
             else:
                 rowVals.append("")  #for Biographical Text. We'll change this later once we have something to put.
             rowVals.append(creator)
@@ -124,6 +157,12 @@ def main():
                # eventType = row["Activity/Organization Name"] + " as a " + row["Position"] + " in " + row["Location"]
                 eventType = row["Position"] + " of " + row["Activity/Organization Name"] + " (in " + row["Location"] + ")."
                 rowVals.append(eventType)
+
+            if (tableType == "sources"):
+                URL = row["Link"]
+                rowVals.append(URL)
+                rowVals.append(row["Publication Date"])
+                rowVals.append(row["Citations"])
 
             lineCount += 1
 
